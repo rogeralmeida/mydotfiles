6b49a9b9ca62482ef2f02752a25ff6d473eaaff7
Roger Almeida <roger.eduardo@gmail.com>
2020-07-09 23:28:28 +1000

    :bug: fixed a few zsh directories

--- mac.sh
+++ mac.sh
@@ -47,8 +47,9 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 echo "Installing my favourite oh-my-zsh plugins"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${$ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 
 ln -s ~/mydofiles/.zshrc ~/.zshrc
 
