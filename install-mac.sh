d77df783523cb351a789037faf93630c35a78f9f
Roger Almeida <roger.eduardo@gmail.com>
2021-12-04 11:06:55 +1100

    :bug: Another way to install xcode cli tools

--- install-mac.sh
+++ install-mac.sh
@@ -14,8 +14,22 @@ then
   abort "Bash is required to interpret this script."
 fi
 
-echo "Installing xcode cli tools"
-xcode-select --install
+echo " Ask for the administrator password upfront."
+sudo -v
+
+# Requires root
+os=$(sw_vers -productVersion | awk -F. '{print $1 "." $2}')
+if softwareupdate --history | grep --silent "Command Line Tools.*${os}"; then
+    echo 'Command-line tools already installed.'
+else
+    echo 'Installing Command-line tools...'
+    in_progress=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    touch ${in_progress}
+    product=$(softwareupdate --list | awk "/\* Command Line.*${os}/ { sub(/^   \* /, \"\"); print }")
+    softwareupdate --verbose --install "${product}" || echo 'Installation failed.' 1>&2 && rm ${in_progress} && exit 1
+    rm ${in_progress}
+    echo 'Installation succeeded.'
+fi
 
 echo "git clone mydotfiles"
 git clone git@github.com:rogeralmeida/mydotfiles.git ~/mydotfiles
